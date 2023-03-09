c33401ffc5cce069cdec0429d08f81f5d8aea95d
Sanjaya <sanjaya.jayasooriya@gmail.com>
2023-03-09 13:10:26 -0600

    Assignment W2D4 - Homework

---
+++ hw9/maze.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $("#start").click(start);
+});
+
+let colorBoundaries = (color) => {
+    $(".boundary").each(function(){
+        this.style.backgroundColor = color
+    });
+}
+
+let start = () => {
+    colorBoundaries("");
+    $(".boundary").mouseenter( boundaryMouseEnter ).mouseleave( boundaryMouseLeave );
+    $("#maze").mouseleave(lose);
+    $("#end").mouseenter(win);
+    $("#start").unbind("click");
+    $("#end").click(stop);
+    $("#status").text("Click the \"S\" to begin.");
+}
+let stop = () => {
+    $(".boundary").unbind("mouseenter");
+    $("#maze").unbind("mouseleave");
+    $(".boundary").unbind("mouseleave");
+    $("#start").click(start);
+    $("#end").unbind("click");
+}
+let boundaryMouseEnter = (eventObject) => {
+    eventObject.target.style.backgroundColor ="red";
+    lose();
+}
+let boundaryMouseLeave = (eventObject) => {
+    eventObject.target.style.backgroundColor = "";
+}
+let lose = () => {
+    colorBoundaries("red");
+    $("#status").text("Sorry, you lost! :[ Click the \"S\" again to begin.");
+    stop();
+}
+let win = () => {
+    $("#status").text("You Win! :] Click the \"S\" again to begin.");
+    stop();
+}
